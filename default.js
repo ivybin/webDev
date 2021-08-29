1880b6b4f8cb022c9662dbdd4622261166376349
IVYS <ivysilva2017@outlook.com>
2021-08-28 21:10:01 -0300

    PROJECTOWEB

---
+++ default.js
@@ -0,0 +1,11 @@
+function addform(){
+    const pesq=document.getElementById("formulario");
+    if (pesq.classList) pesq.classList.add("epi");
+    else pesq.classList += "epi";
+    console.log(formulario);
+    
+}
+function confirm(){
+    const nome=document.getElementById("nome");
+    console.log(nome);
+}
